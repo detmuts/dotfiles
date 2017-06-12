999488ae811fa08369cc860162db8c39483f0344
detvdl <detlev.vandaele@gmail.com>
2017-06-12 22:37:58 +0200

    Slowly but surely getting the hang of pure emacs

--- emacs/.emacs.d/personal/1-init.el
+++ emacs/.emacs.d/personal/1-init.el
@@ -2,11 +2,23 @@
 ;; General packages
 (use-package linum-relative
   :ensure t)
+(global-linum-mode)
 (use-package iedit
   :ensure t)
-;; TODO: Set keymap for multiple-cursors
 (use-package multiple-cursors
   :ensure t)
+(use-package persp-mode
+  :ensure t)
+(use-package pos-tip
+  :ensure t)
+(use-package company-quickhelp
+  :ensure t)
+(company-quickhelp-mode 1)
+(setq company-quickhelp-delay 0.3)
+(use-package ruby-tools
+  :ensure t
+  :bind (:map ruby-tools-mode-map
+         ("C-;" . iedit-mode)))
 (use-package helm-swoop
   :ensure t
   :bind (("M-i" . helm-swoop)
@@ -16,11 +28,26 @@
          :map isearch-mode-map
          ("M-i" . helm-swoop-from-isearch)
          :map helm-swoop-map
-         ("M-i" . helm-multi-swoop-all-from-helm-swoop)))
+         ("M-i" . helm-multi-swoop-all-from-helm-swoop))
+  :config (progn
+            (setq helm-swoop-split-with-multiple-windows t)
+            (setq helm-swoop-split-direction 'split-window-vertically)))
 (use-package paradox
   :ensure t)
 (paradox-enable)
 (use-package rust-mode
+  :ensure t)
+(use-package rvm
+  :ensure t)
+(rvm-use-default)
+(use-package robe
+  :ensure t)
+(add-hook 'ruby-mode-hook 'robe-mode)
+(eval-after-load 'company
+  '(push 'company-robe company-backends))
+(defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
+  (rvm-activate-corresponding-ruby))
+(use-package indent-guide
   :ensure t)
 
 ;; Theming
@@ -30,16 +57,34 @@
 (use-package gruvbox-theme
   :ensure t
   :defer t)
+(load-theme 'gruvbox t)
+(use-package color-theme-sanityinc-tomorrow
+  :ensure t
+  :defer t)
 (use-package org-bullets
   :ensure t)
 (add-hook 'org-mode-hook
           (lambda () (org-bullets-mode 1)))
 (setq org-src-fontify-natively t)
+
+(add-hook 'before-save-hook (lambda ()
+                              (delete-trailing-whitespace)))
+(yas-global-mode 1)
+;; Add yasnippet support for all company backends
+;; https://github.com/syl20bnr/spacemacs/pull/179
+(defvar company-mode/enable-yas t
+  "Enable yasnippet for all backends.")
+(defun company-mode/backend-with-yas (backend)
+  (if (or (not company-mode/enable-yas)
+          (and (listp backend) (member 'company-yasnippet backend)))
+      backend
+    (append (if (consp backend) backend (list backend))
+            '(:with company-yasnippet))))
+(setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
 (setq helm-move-to-line-cycle-in-source nil)
 (scroll-bar-mode -1)
-(global-linum-mode)
 (global-visual-line-mode)
-(load-theme 'gruvbox t)
+(indent-guide-global-mode)
 (setq browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "firefox-nightly")
 ;; (setq guru-warn-only nil)
